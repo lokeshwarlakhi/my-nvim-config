87fac8883413db882fb9abc00ca4646cb379d98b
Developer <dev@example.com>
2026-05-23 21:47:25 +0530

    refactor: replace helpers.lua dependency with native vim.keymap.set

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -9,192 +9,90 @@
 -- - Refactoring (rename, code actions, imports)
 -- - Information (hover, signature, symbols)
 -- - Diagnostics (errors, warnings, fixes)
---
--- Standard conventions:
--- - gd      = Go to Definition (Vim standard)
--- - gr      = Go to References
--- - gi      = Go to Implementation
--- - K       = Hover documentation
--- - <leader>rn = Rename symbol
--- - <leader>ca = Code actions
---
 -- ============================================================================
 
 return function(client, bufnr)
-  local utils = require("utils.helpers")
+  -- Concise mapping helper
+  local function map(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, buffer = bufnr, desc = desc })
+  end
   
   -- =========================================================================
   -- NAVIGATION
   -- =========================================================================
   
-  -- Go to definition
-  utils.keymap("n", "gd", vim.lsp.buf.definition, {
-    desc = "Go to definition",
-    buffer = bufnr,
-  })
-  
-  -- Go to declaration
-  utils.keymap("n", "gD", vim.lsp.buf.declaration, {
-    desc = "Go to declaration",
-    buffer = bufnr,
-  })
-  
-  -- Go to implementation
-  utils.keymap("n", "gi", vim.lsp.buf.implementation, {
-    desc = "Go to implementation",
-    buffer = bufnr,
-  })
-  
-  -- Go to type definition
-  utils.keymap("n", "gt", vim.lsp.buf.type_definition, {
-    desc = "Go to type definition",
-    buffer = bufnr,
-  })
-  
-  -- Go to references
-  utils.keymap("n", "gr", vim.lsp.buf.references, {
-    desc = "Show references",
-    buffer = bufnr,
-  })
+  map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+  map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+  map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+  map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
+  map("n", "gr", vim.lsp.buf.references, "Show references")
   
   -- Incoming calls (functions that call this function)
-  if client.supports_method("textDocument/callHierarchy/incomingCalls") then
-    utils.keymap("n", "<leader>li", vim.lsp.buf.incoming_calls, {
-      desc = "LSP incoming calls",
-      buffer = bufnr,
-    })
+  if client:supports_method("textDocument/callHierarchy/incomingCalls") then
+    map("n", "<leader>li", vim.lsp.buf.incoming_calls, "LSP incoming calls")
   end
   
   -- Outgoing calls (functions this calls)
-  if client.supports_method("textDocument/callHierarchy/outgoingCalls") then
-    utils.keymap("n", "<leader>lo", vim.lsp.buf.outgoing_calls, {
-      desc = "LSP outgoing calls",
-      buffer = bufnr,
-    })
+  if client:supports_method("textDocument/callHierarchy/outgoingCalls") then
+    map("n", "<leader>lo", vim.lsp.buf.outgoing_calls, "LSP outgoing calls")
   end
   
   -- =========================================================================
   -- INFORMATION / DOCUMENTATION
   -- =========================================================================
   
-  -- Hover documentation
-  utils.keymap("n", "K", vim.lsp.buf.hover, {
-    desc = "Hover documentation",
-    buffer = bufnr,
-  })
-  
-  -- Signature help
-  utils.keymap("i", "<C-k>", vim.lsp.buf.signature_help, {
-    desc = "Signature help",
-    buffer = bufnr,
-  })
-  
-  -- Show line diagnostics
-  utils.keymap("n", "<leader>d", vim.diagnostic.open_float, {
-    desc = "Show diagnostics",
-    buffer = bufnr,
-  })
-  
-  -- Previous diagnostic
-  utils.keymap("n", "[d", vim.diagnostic.goto_prev, {
-    desc = "Previous diagnostic",
-    buffer = bufnr,
-  })
-  
-  -- Next diagnostic
-  utils.keymap("n", "]d", vim.diagnostic.goto_next, {
-    desc = "Next diagnostic",
-    buffer = bufnr,
-  })
+  map("n", "K", vim.lsp.buf.hover, "Hover documentation")
+  map("i", "<C-k>", vim.lsp.buf.signature_help, "Signature help")
+  map("n", "<leader>d", vim.diagnostic.open_float, "Show diagnostics")
+  map("n", "[d", vim.diagnostic.goto_prev, "Previous diagnostic")
+  map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
   
   -- =========================================================================
   -- REFACTORING / CODE ACTIONS
   -- =========================================================================
   
-  -- Rename symbol
-  utils.keymap("n", "<leader>rn", vim.lsp.buf.rename, {
-    desc = "Rename symbol",
-    buffer = bufnr,
-  })
-  
-  -- Code actions
-  utils.keymap("n", "<leader>ca", vim.lsp.buf.code_action, {
-    desc = "Code actions",
-    buffer = bufnr,
-  })
+  map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+  map("n", "<leader>ca", vim.lsp.buf.code_action, "Code actions")
   
   -- Organize imports (language-specific)
-  if client.supports_method("textDocument/codeAction") then
-    utils.keymap("n", "<leader>co", function()
+  if client:supports_method("textDocument/codeAction") then
+    map("n", "<leader>co", function()
       vim.lsp.buf.code_action({
         apply = true,
         context = {
           only = { "source.organizeImports" },
         },
       })
-    end, {
-      desc = "Organize imports",
-      buffer = bufnr,
-    })
+    end, "Organize imports")
   end
   
   -- Fix diagnostic at cursor (quick fix)
-  if client.supports_method("textDocument/codeAction") then
-    utils.keymap("n", "<leader>cf", function()
+  if client:supports_method("textDocument/codeAction") then
+    map("n", "<leader>cf", function()
       vim.lsp.buf.code_action({
         apply = true,
         context = {
           diagnostics = { vim.diagnostic.get_cursor() },
         },
       })
-    end, {
-      desc = "Fix diagnostic",
-      buffer = bufnr,
-    })
+    end, "Fix diagnostic")
   end
   
-  -- =========================================================================
-  -- FORMATTING
-  -- =========================================================================
-  
-  -- Format buffer
-  if client.supports_method("textDocument/formatting") then
-    utils.keymap("n", "<leader>lf", function()
-      vim.lsp.buf.format({ async = true })
-    end, {
-      desc = "Format buffer",
-      buffer = bufnr,
-    })
-  end
-  
-  -- Format selection (visual mode)
-  if client.supports_method("textDocument/rangeFormatting") then
-    utils.keymap("v", "<leader>lf", function()
-      vim.lsp.buf.format({ async = true })
-    end, {
-      desc = "Format selection",
-      buffer = bufnr,
-    })
-  end
+  -- Note: Formatting (<leader>lf) is handled by conform.nvim globally 
+  -- to avoid overriding with fallback LSP formatting.
   
   -- =========================================================================
   -- SYMBOLS / OUTLINE
   -- =========================================================================
   
   -- Document symbols (show all symbols in current file)
-  if client.supports_method("textDocument/documentSymbol") then
-    utils.keymap("n", "<leader>fs", vim.lsp.buf.document_symbol, {
-      desc = "Document symbols",
-      buffer = bufnr,
-    })
+  if client:supports_method("textDocument/documentSymbol") then
+    map("n", "<leader>fs", vim.lsp.buf.document_symbol, "Document symbols")
   end
   
   -- Workspace symbols (search all symbols in project)
-  if client.supports_method("workspace/symbol") then
-    utils.keymap("n", "<leader>fS", vim.lsp.buf.workspace_symbol, {
-      desc = "Workspace symbols",
-      buffer = bufnr,
-    })
+  if client:supports_method("workspace/symbol") then
+    map("n", "<leader>fS", vim.lsp.buf.workspace_symbol, "Workspace symbols")
   end
   
   -- =========================================================================
@@ -202,35 +100,19 @@ return function(client, bufnr)
   -- =========================================================================
   
   -- Add folder to workspace
-  if client.supports_method("workspace/didChangeWatchedFiles") then
-    utils.keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, {
-      desc = "Add workspace folder",
-      buffer = bufnr,
-    })
+  if client:supports_method("workspace/didChangeWatchedFiles") then
+    map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
   end
   
   -- Remove folder from workspace
-  if client.supports_method("workspace/didChangeWatchedFiles") then
-    utils.keymap("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, {
-      desc = "Remove workspace folder",
-      buffer = bufnr,
-    })
+  if client:supports_method("workspace/didChangeWatchedFiles") then
+    map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
   end
   
   -- List workspace folders
-  if client.supports_method("workspace/didChangeWatchedFiles") then
-    utils.keymap("n", "<leader>wl", function()
+  if client:supports_method("workspace/didChangeWatchedFiles") then
+    map("n", "<leader>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, {
-      desc = "List workspace folders",
-      buffer = bufnr,
-    })
+    end, "List workspace folders")
   end
-  
-  -- =========================================================================
-  -- HIGHLIGHTS & REFERENCES
-  -- =========================================================================
-  
-  -- Clear highlight on CursorMoved (handled in lsp/config.lua)
-  -- Document highlight is also handled automatically in config.lua
 end
