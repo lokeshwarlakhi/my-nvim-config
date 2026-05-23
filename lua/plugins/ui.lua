207f7c560d6c724f8791d8f07057df8e261037e0
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2026-05-23 23:57:18 +0530

    feat: add neoscroll.nvim for smooth scrolling animations

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -137,5 +137,21 @@ return {
       -- Find all TODO comments using Telescope helper
       vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find TODOs in project" })
     end
+  },
+
+  -- Smooth Scrolling (neoscroll.nvim)
+  {
+    "karb94/neoscroll.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("neoscroll").setup({
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+        hide_cursor = true,
+        stop_eof = true,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = "quadratic",
+      })
+    end
   }
 }
